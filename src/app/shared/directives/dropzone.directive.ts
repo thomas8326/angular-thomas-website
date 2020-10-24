3ac1f6dc80b94189743cf52356a09bf8945d8968
Thomas Shih <thomasshih@Thomass-MacBook-Air.local>
2020-10-24 23:37:54 +0800

    style: complete the dropzone

--- src/app/shared/directives/dropzone.directive.ts
+++ src/app/shared/directives/dropzone.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, Host } from '@angular/core';
 
 @Directive({
   selector: '[appDropzone]'
 })
 export class DropzoneDirective {
 
-  constructor() { }
+  @HostListener('dragenter', ['$event']) onDragenter() {
+    this.renderer2.addClass(this.elementRef.nativeElement, 'dropzone');
+  }
 
+  @HostListener('dragleave', ['$event']) onDragleave() {
+    this.renderer2.removeClass(this.elementRef.nativeElement, 'dropzone');
+  }
+
+  constructor(private readonly elementRef: ElementRef,
+    private readonly renderer2: Renderer2) { }
 }
