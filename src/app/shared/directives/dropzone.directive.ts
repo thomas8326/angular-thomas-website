27c2714d2cf2c2013c08cd31d63ee76c37292b69
Thomas Shih <thomasshih@Thomass-MacBook-Air.local>
2020-10-25 11:12:33 +0800

    feat: dropzone use observable

--- src/app/shared/directives/dropzone.directive.ts
+++ src/app/shared/directives/dropzone.directive.ts
@@ -1,18 +1,68 @@
-import { Directive, HostListener, ElementRef, Renderer2, Host } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, Host, Output, EventEmitter } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { BehaviorSubject } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Directive({
   selector: '[appDropzone]'
 })
 export class DropzoneDirective {
 
-  @HostListener('dragenter', ['$event']) onDragenter() {
-    this.renderer2.addClass(this.elementRef.nativeElement, 'dropzone');
+  @Output() fileDropped = new EventEmitter<FileList>()
+
+  private readonly dropzone$$ = new BehaviorSubject(false);
+
+  @HostListener('dragenter', ['$event']) onDragenter(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropzone$$.next(true);
+    // this.createZone();
+
   }
 
-  @HostListener('dragleave', ['$event']) onDragleave() {
+  @HostListener('dragover', ['$event']) onDragover(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropzone$$.next(true);
+    // this.createZone();
+  }
+
+  @HostListener('dragleave', ['$event']) onDragleave(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(event);
+    this.dropzone$$.next(false)
+    // this.removeZone();
+  }
+
+  @HostListener('drop', ['$event']) onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropzone$$.next(false);
+    this.removeZone();
+    this.fileDropped.emit(event.dataTransfer.files);
+  }
+
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer2: Renderer2,
+  ) {
+    this.dropzone$$.pipe(distinctUntilChanged()).subscribe(isShowdropzone => {
+      if (isShowdropzone) {
+        this.createZone();
+      } else {
+        this.removeZone();
+      }
+    })
+
+  }
+
+  private removeZone() {
     this.renderer2.removeClass(this.elementRef.nativeElement, 'dropzone');
   }
 
-  constructor(private readonly elementRef: ElementRef,
-    private readonly renderer2: Renderer2) { }
+  private createZone() {
+    this.renderer2.addClass(this.elementRef.nativeElement, 'dropzone');
+  }
 }
