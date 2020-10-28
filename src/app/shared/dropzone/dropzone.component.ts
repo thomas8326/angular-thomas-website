fb44f12fa3891a98b72d89d306322558b8007288
Thomas Shih <thomasshih@Thomass-MacBook-Air.local>
2020-10-28 21:38:13 +0800

    feat: create drop zone component

--- src/app/shared/dropzone/dropzone.component.ts
+++ src/app/shared/dropzone/dropzone.component.ts
@@ -1,30 +1,41 @@
-import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+
 import { fromEvent, merge } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { first, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dropzone',
-  template: '<ng-content></ng-content>',
+  templateUrl: './dropzone.component.html',
   styleUrls: ['./dropzone.component.scss'],
 })
-export class DropzoneComponent implements OnInit {
+export class DropzoneComponent implements AfterViewInit {
+  @ViewChild('dropzone') dropzone: ElementRef<any>;
+  showOverlay = false;
+
   constructor(private readonly elementRef: ElementRef, private readonly renderer2: Renderer2) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     fromEvent(this.elementRef.nativeElement, 'dragenter')
       .pipe(
-        tap(() => console.log('enter')),
+        tap((event: DragEvent) => {
+          this.showOverlay = true;
+          console.log('enter');
+        }),
         switchMap(() =>
           merge(
-            fromEvent(this.elementRef.nativeElement, 'dragleave'),
-            fromEvent(this.elementRef.nativeElement, 'drop'),
-          ),
+            fromEvent(this.dropzone.nativeElement, 'dragleave'),
+            fromEvent(this.dropzone.nativeElement, 'drop'),
+          ).pipe(first()),
         ),
       )
       .subscribe((event: DragEvent) => {
         event.preventDefault();
         event.stopPropagation();
-        console.log(event);
+        this.showOverlay = false;
       });
+
+    fromEvent(this.dropzone.nativeElement, 'dragover').subscribe((event: DragEvent) => {
+      event.preventDefault();
+    });
   }
 }
